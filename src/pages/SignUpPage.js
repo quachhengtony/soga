cef9dc38163e9206bca65cc3d0d6d5245381decf
TonyInfinity <quachhengtony@gmail.com>
2021-02-13 21:50:03 +0700

    Reload app after sign up

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -42,6 +42,7 @@ function SignUpPage() {
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       });
       await signUp(emailRef.current.value, passwordRef.current.value);
+      window.location.reload();
       history.push("/links");
     } catch {
       setError("Failed to create account.");
