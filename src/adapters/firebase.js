26efc5d162a768701753dab437ca64f0acff8db5
TonyInfinity <quachhengtony@gmail.com>
2021-02-13 22:16:55 +0700

    Reinstalled packages

--- src/adapters/firebase.js
+++ src/adapters/firebase.js
@@ -2,14 +2,6 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyB1IGg_0RSwub1DI1J8EiPtiF3GSp7q-ng",
-  // authDomain: "soga-7120a.firebaseapp.com",
-  // databaseURL: "https://soga-7120a.firebaseio.com",
-  // projectId: "soga-7120a",
-  // storageBucket: "soga-7120a.appspot.com",
-  // messagingSenderId: "405174960922",
-  // appId: "1:405174960922:web:4bf6e633daaa20671d62a3",
-  // measurementId: "G-YFXEB6ZPCK",
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
